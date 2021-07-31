4f58a69f344a17729c30504d6a415456303ab1ef
Denver-Meadows <meadows8200@gmail.com>
2021-07-31 17:41:45 -0400

    State and API working

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const dogImage = document.querySelector('.dog__image');
 const reloadBtn = document.querySelector('.dog__reload-btn');
 
+const state = {
+  breeds: '',
+}
+
 const getDogBreedList = async function() {
   try {
-
     const request = fetch('https://dog.ceo/api/breeds/list/all');
     const res = await request;
     const data = await res.json();
 
     console.log(data)
-    console.log(data.message)
+    state.breeds = data.message
+    console.log(state.breeds)
+    console.log(Object.keys(state.breeds))
     
   } catch (error) {
     console.error(error)
@@ -27,14 +32,23 @@ const getDogBreedPhoto = async function(breed) {
     console.log(data.message)
     console.log(data.message.length)
 
+    // Get random image based on the number of photos per breed
     dogImage.src = data.message[Math.floor(Math.random() * data.message.length)]
 
   } catch (error) {
     console.error(error)
   }
-}
+};
 
+
+
+// Reload to pick a random photo of the current breed.
 reloadBtn.addEventListener('click', () => location.reload())
 
-getDogBreedList();
-getDogBreedPhoto('corgi');
+const init = function() {
+  getDogBreedList();
+};
+
+init();
+
+// getDogBreedPhoto('corgi');
