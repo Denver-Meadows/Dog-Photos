b28411c016cebc37dccea3f58cb4fed1dc4746ba
Denver-Meadows <meadows8200@gmail.com>
2021-07-31 18:00:58 -0400

    Drop down fully working with API dog breeds

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dogImage = document.querySelector('.dog__image');
 const reloadBtn = document.querySelector('.dog__reload-btn');
+const dropdown = document.querySelector('.dog__form__dropdown');
 
 const state = {
   breeds: '',
@@ -12,9 +13,12 @@ const getDogBreedList = async function() {
     const data = await res.json();
 
     console.log(data)
-    state.breeds = data.message
+    state.breeds = Object.keys(data.message)
     console.log(state.breeds)
-    console.log(Object.keys(state.breeds))
+
+    // ** I need a function that will dynamically add the breeds to drop down
+    // *** First i will just try to add anything into the drop down
+    renderBreedOptions(state.breeds)
     
   } catch (error) {
     console.error(error)
@@ -41,6 +45,16 @@ const getDogBreedPhoto = async function(breed) {
 };
 
 
+
+
+
+const renderBreedOptions = function(data) {
+
+  data.forEach(dog => {
+    dropdown.insertAdjacentHTML('beforeend', `<option value ="">${dog}</option>`)
+  })
+  
+}
 
 // Reload to pick a random photo of the current breed.
 reloadBtn.addEventListener('click', () => location.reload())
