298d79e9b8b40e258492613fcc8aa285400e7040
Denver-Meadows <meadows8200@gmail.com>
2021-07-31 13:10:30 -0400

    API working

---
+++ script.js
@@ -0,0 +1,37 @@
+const dogImage = document.querySelector('.dog__image');
+
+const getDogBreedList = async function() {
+  try {
+
+    const request = fetch('https://dog.ceo/api/breeds/list/all');
+    const res = await request;
+    const data = await res.json();
+
+    console.log(data)
+    console.log(data.message)
+    
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const getDogBreedPhoto = async function(breed) {
+  try {
+
+    const request = fetch(`https://dog.ceo/api/breed/${breed}/images`);
+    const res = await request;
+    const data = await res.json();
+
+    console.log(data)
+    console.log(data.message)
+    console.log(data.message.length)
+
+    dogImage.src = data.message[Math.floor(Math.random() * data.message.length)]
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+getDogBreedList();
+getDogBreedPhoto('corgi');
